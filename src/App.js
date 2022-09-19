47f295f56558481fc8e351b2f6fdc25cb2f8742b
afolabiopakunle <opakunleafolabi@gmail.com>
2022-09-19 11:44:58 +0100

    useReducer level one done

--- src/App.js
+++ src/App.js
@@ -2,9 +2,30 @@ import React, { useReducer } from "react";
 import "./style.css";
 
 export default function App() {
+
+  function reducer(state, action) {
+    if(action.type === 'increment') {
+      return {count: state.count++}
+    }
+
+    if(action.type === 'decrement') {
+      return {count: state.count--}
+    }
+  }
+
+  const [state, dispatch] = useReducer(reducer, {count: 0});
+
+  function decrement() {
+    dispatch({type: 'decrement'})
+  }
+
+  function increment() {
+    dispatch({type: 'increment'})
+  }
+
   return (
     <div>
-  
+      <button onClick={decrement}>-</button><span>{state.count}</span><button onClick={increment}>+</button>
     </div>
   );
 }
